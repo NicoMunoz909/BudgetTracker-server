75f027a729b871a729db5c2a11ee3880bda41703
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-04-05 10:20:20 -0300

    Write middleware validations

--- server/router/operations.js
+++ server/router/operations.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const controller = require('../controllers/operations.js');
+const validations = require('../validations/operations.js')
 
 const router = express.Router();
 
@@ -11,10 +12,15 @@ const {
   deleteOperation
 } = controller;
 
-router.post('/', createOperation);
+const {
+  validateRequiredFields,
+  validateFormat
+} = validations;
+
+router.post('/',validateRequiredFields,validateFormat, createOperation);
 router.delete('/:id', deleteOperation);
 router.get('/', getAll);
 router.get('/:id', getById);
-router.patch('/:id', updateOperation);
+router.patch('/:id',validateFormat, updateOperation);
 
 module.exports = router;
