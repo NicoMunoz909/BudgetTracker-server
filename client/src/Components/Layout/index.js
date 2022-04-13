c96542862eca76d41e8c56da6997306dda71a40d
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-04-13 10:19:14 -0300

    Modify layout css

--- client/src/Components/Layout/index.js
+++ client/src/Components/Layout/index.js
@@ -1,11 +1,13 @@
 import styles from './layout.module.css'
 import Sidebar from '../Sidebar/index.js'
 import Footer from '../Footer/index.js'
+import Home from '../Home'
 
 const Layout = () => {
   return (
     <div className={styles.container}>
       <Sidebar />
+      <Home />
       <Footer />
     </div>
   )
