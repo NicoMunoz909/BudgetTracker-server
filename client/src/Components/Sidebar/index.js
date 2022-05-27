020ce6975a6b65ed1a4e872335d7b5699fe352d7
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-05-27 10:42:05 -0300

    Make site responsive

--- client/src/Components/Sidebar/index.js
+++ client/src/Components/Sidebar/index.js
@@ -6,10 +6,12 @@ const Sidebar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <BsCalculator className={styles.icon}/>
+        <div className={styles.svgContainer}>
+          <BsCalculator className={styles.icon}/>
+        </div>
         <h1 className={styles.title}>Budget Tracker</h1>
       </div>
-      <div>
+      <div className={styles.links}>
         <NavLink className={({ isActive }) => isActive ? styles.link + ' ' + styles.selected : styles.link} to="/" >home</NavLink>
         <NavLink className={({ isActive }) => isActive ? styles.link + ' ' + styles.selected : styles.link} to="/moves" >All moves</NavLink>
       </div>
