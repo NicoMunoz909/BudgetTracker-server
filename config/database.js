4667baf98c3a4020160239a8493025087b7384b0
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-06-01 08:47:34 -0300

    Set up Heroku ClearDB database

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 module.exports = mysql.createConnection({
-  host: 'localhost',
-  user: 'alkemy',
-  password: 'alkemy',
-  database: 'alkemy',
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b80da5b4aa2aec',
+  password: '4118861c',
+  database: 'heroku_b31a2aae1c2207a',
 })
 
